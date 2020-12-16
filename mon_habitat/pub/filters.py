bce929f9140c265c4abbd13ddb84befb92458fb1
willdev <willdev61@gmail.com>
2020-12-16 21:24:28 +0000

    migrations

--- mon_habitat/pub/filters.py
+++ mon_habitat/pub/filters.py
@@ -4,11 +4,13 @@ from django_filters import  ChoiceFilter
 
 
 class PublicationFilter(django_filters.FilterSet):
-    cities = ChoiceFilter(choices=CITIES_CHOICES)
-    categories = ChoiceFilter(choices=CATEGORIES_CHOICES)
-    status = ChoiceFilter(choices=STATUS_CHOICES)
     class Meta:
         model = Publication
         fields =[
             'cities', 'categories', 'status'
         ]
+
+
+# cities = ChoiceFilter(choices=CITIES_CHOICES)
+    # categories = ChoiceFilter(choices=CATEGORIES_CHOICES)
+    # status = ChoiceFilter(choices=STATUS_CHOICES)
